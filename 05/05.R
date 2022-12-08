0a490450723fdcfc33d798542957819de8fb09ad
Freya Boardman-Pretty <freya@mendelian.co>
2022-12-08 10:25:37 +0000

    day 5 part 2

--- 05/05.R
+++ 05/05.R
@@ -1,5 +1,4 @@
 # day 5: supply stacks
-
 require(tidyverse)
 
 example_input <- read_csv("05/example_input.txt",
@@ -14,76 +13,88 @@ input <- read_csv("05/input.txt",
                   trim_ws = FALSE
 )
 
-input <- example_input
-
-# split the input into the two parts
+# create the stacks data
 stacks <- input %>%
-  filter(!grepl("^move", X1))
+  filter(!grepl("^move", X1)) %>%
+  map_df(., rev)
 
-moves <- input %>%
-  filter(grepl("^move", X1))
-
-# sort out the stacks
-# reverse the rows
-stacks <- map_df(stacks, rev)
-
-# separate into cols
+# get name and number of stacks
 name_stacks <- gsub(" ","", stacks$X1[[1]]) %>%
   strsplit(., "") %>%
   unlist() %>%
   paste0("s", .)
-  
-# get number of stacks
+
 num_stacks <- length(name_stacks)
 
 stacks <- stacks %>%
   separate(X1,
            into = name_stacks,
-           sep = seq(from = 4, to = 4*(num_stacks-1), by = 4)
-  ) %>%
+           sep = seq(from = 4, to = 4*(num_stacks-1), by = 4)) %>%
   slice(-1) %>% 
   mutate(across(everything(), str_trim),
          across(everything(), ~gsub("\\[|\\]", "", .)),
-           across(everything(), ~na_if(., "")))
+           across(everything(), ~na_if(., ""))) %>%
+  as.list() %>%
+  map(., ~ as.character(na.omit(.x)))
 
-# rows to a list
-stacks <- as.list(stacks)
-
-# remove the NAs
-stacks <- map(stacks, ~ as.character(na.omit(.x)))
-
-# look at the moves
-moves <- moves %>%
+# create the moves data
+moves <- input %>%
+  filter(grepl("^move", X1)) %>%
   mutate(X1 = gsub("move ","",X1)) %>%
   separate(X1, into = c("repeat", "from", "to"), sep = " from | to ") %>%
   mutate(across(everything(), as.numeric))
 
+# -------- part 1 -------
+
 # make a mover function
-move_crate <- function(stax, from_stack, to_stack){
-  
-  crate <- tail(stax[[from_stack]], 1)
-  
-  stax[[from_stack]] <- head(stax[[from_stack]], -1)
-  
-  stax[[to_stack]] <- append(stax[[to_stack]], crate)
- 
-  return(stax)
+move_crate <- function(stacks, from_stack, to_stack){
+  crate <- tail(stacks[[from_stack]], 1)
+  stacks[[from_stack]] <- head(stacks[[from_stack]], -1)
+  stacks[[to_stack]] <- append(stacks[[to_stack]], crate)
+  return(stacks)
 }
 
-move_crate_sequence <- function(reps, from, to) {
-  
+move_crate_sequence <- function(stacks, reps, from, to) {
   for(i in 1:reps) {
     stacks <- move_crate(stacks, from, to)
   }
- 
   return(stacks) 
 }
 
+stacks_p1 <- stacks
 
+# move the crates
 for (i in 1:nrow(moves)){
-  stacks <- move_crate_sequence(reps = moves[[1]][i],
-                      from = moves[[2]][i],
-                      to   = moves[[3]][i])
+  stacks_p1 <- move_crate_sequence(stacks = stacks_p1,
+                                reps = moves[[1]][i],
+                                from = moves[[2]][i],
+                                to   = moves[[3]][i])
 }
 
-map(stacks, ~ tail(.x, 1)) %>% unlist() %>% paste(collapse = "")
+map(stacks_p1, ~ tail(.x, 1)) %>% unlist() %>% paste(collapse = "")
+
+# -------- part 2 --------
+
+# make a mover function to move multiple crates at once
+
+move_crates_together <- function(stacks, from_stack, to_stack, num_crates){
+  
+  crates <- tail(stacks[[from_stack]], num_crates)
+  
+  stacks[[from_stack]] <- head(stacks[[from_stack]], -num_crates)
+  
+  stacks[[to_stack]] <- append(stacks[[to_stack]], crates)
+  
+  return(stacks)
+}
+
+stacks_p2 <- stacks
+
+for (i in 1:nrow(moves)){
+  stacks_p2 <- move_crates_together(stacks = stacks_p2,
+                                from = moves[[2]][i],
+                                to   = moves[[3]][i],
+                                num_crates = moves[[1]][i])
+}
+
+map(stacks_p2, ~ tail(.x, 1)) %>% unlist() %>% paste(collapse = "")
